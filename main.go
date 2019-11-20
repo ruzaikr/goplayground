98543ce7a2c6798c1a1ac0d5d0ae15751a4d3280
Ruzaik Rafeek <ruzaik.rafeek@lalamove.com>
2019-11-21 02:27:10 +0800

    test code

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
+func getDataFromThirdPartyHTTPAPI(url string) (*http.Response, int, error) {
+	var resp, _ = http.Get(url)
+
+	if resp.StatusCode == http.StatusNotFound {
+		return nil, http.StatusNotFound, fmt.Errorf("third party HTTP API responded with 404 StatusNotFound")
+	}
+	return resp, http.StatusOK, nil
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	var productionURL = "https://www.realproductionURL.com"
+
+	var _, statusCode, err = getDataFromThirdPartyHTTPAPI(productionURL)
+
+	if err != nil {
+		log.Printf("Error occurred: %v. Status code: %d", err, statusCode)
+	}else {
+		log.Printf("Call to third party HTTP API was successful")
+	}
 }
