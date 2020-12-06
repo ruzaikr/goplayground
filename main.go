74f41a4748e5a3414dca5caa9824960f687dc31e
ruzaik.rafeek <ruzaik.rafeek@lalamove.com>
2020-12-06 15:36:11 +0800

    initial commit

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"log"
 )
 
+func merge(intervals [][]int) [][]int {
+	if len(intervals) < 2 {
+		return intervals
+	}
+
+	for i := 0; i < len(intervals) - 1; i++ {
+		if intervals[i][1] >= intervals[i+1][0] {
+			intervals[i+1][0] = intervals[i][0]
+			intervals = append(intervals[:i], intervals[i+1:]...)
+		}
+	}
+	return intervals
+}
+
 func main() {
-	fmt.Println("Hello World!!")
+	var myIntervals = [][]int{{1,3},{2,6},{8,10},{15,18}}
+	log.Println(merge(myIntervals))
 }
