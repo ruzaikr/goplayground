2be0a3d5de15d957f8a3961f9d9545cd2cdd375d
ruzaik.rafeek <ruzaik.rafeek@lalamove.com>
2020-12-06 17:40:26 +0800

    Cover test case: [[1,4],[0,4]]

--- main.go
+++ main.go
@@ -1,26 +1,38 @@
 package main
 
-import (
-	"log"
-)
+import "log"
 
-func merge(intervals [][]int) [][]int {
+func removeFromIntervals(i int, intervals [][]int) [][]int {
+	return append(intervals[:i], intervals[i+1:]...)
+}
+
+func isAContainedWithinB(a []int, b []int) bool {
+	return a[0] >= b[0] && a[1] <= b[1]
+}
+
+func Merge(intervals [][]int) [][]int {
 	if len(intervals) < 2 {
 		return intervals
 	}
 
 	for i := 0; i < len(intervals) - 1; i++ {
-		if intervals[i][1] >= intervals[i+1][0] {
+		if isAContainedWithinB(intervals[i], intervals[i+1]) {
+			intervals = removeFromIntervals(i, intervals)
+		}else if intervals[i][1] >= intervals[i+1][0] {
 			intervals[i+1][0] = intervals[i][0]
-			intervals = append(intervals[:i], intervals[i+1:]...)
+			intervals = removeFromIntervals(i, intervals)
 		}
 	}
 	return intervals
 }
 
 func main() {
-	var myIntervals = [][]int{{1,3},{2,6},{8,10},{15,18}}
-	var myIntervals2 = [][]int{{1,4},{4,5}}
-	log.Println(merge(myIntervals))
-	log.Println(merge(myIntervals2))
+	var testCases = [][][]int{
+		{{1,3},{2,6},{8,10},{15,18}},
+		{{1,4},{4,5}},
+	}
+
+	for i := 0; i < len(testCases); i++ {
+		log.Println(Merge(testCases[i]))
+	}
 }
---
+++ main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"testing"
+)
+
+var mergeTests = []struct {
+	name string
+	inputParams  [][]int
+	expectedOutput [][]int
+}{
+	{
+		"Case 1",
+		[][]int{{1,3},{2,6},{8,10},{15,18}},
+		[][]int{{1,6},{8,10},{15,18}}},
+	{
+		"Case 2",
+		[][]int{{1,4},{4,5}},
+		[][]int{{1,5}},
+	},
+	{
+		"Case 3",
+		[][]int{{1,4},{0,4}},
+		[][]int{{0,4}},
+	},
+}
+
+func areIntervalsEqual(a [][]int, b [][]int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := 0; i < len(a); i++ {
+		if a[i][0] != b[i][0] || a[i][1] != b[i][1] {
+			return false
+		}
+	}
+	return true
+}
+
+func TestMerge(t *testing.T) {
+	for _, mergeTest := range mergeTests {
+		t.Run(mergeTest.name, func(t *testing.T) {
+			var actualOutput = Merge(mergeTest.inputParams)
+			if !areIntervalsEqual(actualOutput, mergeTest.expectedOutput) {
+				t.Errorf("got %v, want %v", actualOutput, mergeTest.expectedOutput)
+			}
+		})
+	}
+}
