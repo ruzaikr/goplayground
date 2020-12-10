7bcd7d0f9a9f28440f926a284fce1912215e6836
Ruzaik Rafeek <ruzaik.rafeek@lalamove.com>
2020-12-10 18:31:03 +0800

    brute force solution

--- main.go
+++ main.go
@@ -2,59 +2,24 @@ package main
 
 import (
 	"log"
-	"sort"
 )
 
-type byCustom [][]int
-
-func (intervals byCustom) Len() int {
-	return len(intervals)
-}
-func (intervals byCustom) Swap(i, j int) {
-	intervals[i], intervals[j] = intervals[j], intervals[i]
-}
-func (intervals byCustom) Less(i, j int) bool {
-	return intervals[i][0] < intervals[j][0]
-}
-
-func removeFromIntervals(i int, intervals [][]int) [][]int {
-	return append(intervals[:i], intervals[i+1:]...)
-}
-
-func isAContainedWithinB(a []int, b []int) bool {
-	return a[0] >= b[0] && a[1] <= b[1]
-}
-
-func Merge(intervals [][]int) [][]int {
-	if len(intervals) < 2 {
-		return intervals
+func twoSum(nums []int, target int) []int {
+	if len(nums) < 2 {
+		log.Fatalln("Length of input array must be larger than or equal to 2")
 	}
 
-	sort.Sort(byCustom(intervals))
-
-	for i := 0; i < len(intervals) - 1; i++ {
-		if isAContainedWithinB(intervals[i], intervals[i+1]) {
-			intervals = removeFromIntervals(i, intervals)
-			i--
-		}else if isAContainedWithinB(intervals[i+1], intervals[i]) {
-			intervals = removeFromIntervals(i+1, intervals)
-			i--
-		} else if intervals[i][1] >= intervals[i+1][0] {
-			intervals[i+1][0] = intervals[i][0]
-			intervals = removeFromIntervals(i, intervals)
-			i--
+	for i := 0; i < len(nums) - 1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			var sum = nums[i] + nums[j]
+			if sum == target {
+				return []int{i, j}
+			}
 		}
 	}
-	return intervals
+	return []int{}
 }
 
 func main() {
-	var testCases = [][][]int{
-		{{1,3},{2,6},{8,10},{15,18}},
-		{{1,4},{4,5}},
-	}
 
-	for i := 0; i < len(testCases); i++ {
-		log.Println(Merge(testCases[i]))
-	}
 }
--- main_test.go
+++ main_test.go
@@ -4,71 +4,36 @@ import (
 	"testing"
 )
 
-var mergeTests = []struct {
+var twoSumTests = []struct {
 	name string
-	inputParams  [][]int
-	expectedOutput [][]int
+	inputArray  []int
+	target int
+	expectedOutput []int
 }{
 	{
-		"Case 1",
-		[][]int{{1,3},{2,6},{8,10},{15,18}},
-		[][]int{{1,6},{8,10},{15,18}}},
-	{
-		"Case 2",
-		[][]int{{1,4},{4,5}},
-		[][]int{{1,5}},
-	},
-	{
-		"Case 3",
-		[][]int{{1,4},{0,4}},
-		[][]int{{0,4}},
-	},
-	{
-		"Case 4", // required sorting
-		[][]int{{1,4},{0,1}},
-		[][]int{{0,4}},
-	},
-	{
-		"Case 5",
-		[][]int{{1,4},{2,3}},
-		[][]int{{1,4}},
-	},
-	{
-		"Case 6",
-		[][]int{{1,4},{0,2},{3,5}},
-		[][]int{{0,5}},
-	},
-	{
-		"Case 7",
-		[][]int{{2,3},{4,5},{6,7},{8,9},{1,10}},
-		[][]int{{1,10}},
-	},
-	{
-		"Case 8",
-		[][]int{{1,3},{0,2},{2,3},{4,6},{5,5},{0,2},{3,3}},
-		[][]int{{0,3},{4,6}},
+	"Case 1",
+	[]int{2, 7, 11, 15},
+		9,
+	[]int{0, 1},
 	},
 }
 
-func areIntervalsEqual(a [][]int, b [][]int) bool {
-	if len(a) != len(b) {
-		return false
-	}
-
+func areIntArraysEqual(a []int, b []int) bool {
 	for i := 0; i < len(a); i++ {
-		if a[i][0] != b[i][0] || a[i][1] != b[i][1] {
+		if a[i] != b[i] {
 			return false
 		}
 	}
 	return true
 }
 
-func TestMerge(t *testing.T) {
-	for _, mergeTest := range mergeTests {
-		t.Run(mergeTest.name, func(t *testing.T) {
-			var actualOutput = Merge(mergeTest.inputParams)
-			if !areIntervalsEqual(actualOutput, mergeTest.expectedOutput) {
-				t.Errorf("got %v, want %v", actualOutput, mergeTest.expectedOutput)
+
+func TestTwoSum(t *testing.T) {
+	for _, twoSumTest := range twoSumTests {
+		t.Run(twoSumTest.name, func(t *testing.T) {
+			var actualOutput = twoSum(twoSumTest.inputArray, twoSumTest.target)
+			if !areIntArraysEqual(actualOutput, twoSumTest.expectedOutput) {
+				t.Errorf("got %v, want %v", actualOutput, twoSumTest.expectedOutput)
 			}
 		})
 	}
