9a94467d281ee19f9d311f16294a005de736a195
Ruzaik Rafeek <ruzaik.rafeek@lalamove.com>
2020-12-12 16:15:25 +0800

    Passed first stage in leetcode

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"sort"
 )
 
@@ -11,14 +11,38 @@ type visit struct {
 	website string
 }
 
-
 func mostVisitedPattern(username []string, timestamp []int, website []string) []string {
+	var websitesMap = make(map[string]bool)
+	var websites = make([]string, 0)
 	var visits = make([]visit, len(timestamp))
 	for i := 0; i < len(timestamp); i++ {
 		visits[i] = visit{
 			username: username[i],
 			timestamp: timestamp[i],
 			website: website[i],
+		}
+
+		if _, exists := websitesMap[website[i]]; !exists {
+			websitesMap[website[i]] = true
+			websites = append(websites, website[i])
+		}
+	}
+
+	var threeSeqs = make(map[string]int)
+
+	// make all possible 3-sequences
+	for i := 0; i < len(websites); i++ {
+		for j := i + 1; j < len(websites); j++ {
+			if websites[j] == websites[i] {
+				continue
+			}
+			for k := j + 1; k < len(websites); k++ {
+				if websites[k] == websites[j] {
+					continue
+				}
+				var seq = fmt.Sprintf("%s,%s,%s",websites[i],websites[j],websites[k])
+				threeSeqs[seq] = 0
+			}
 		}
 	}
 
@@ -37,15 +61,32 @@ func mostVisitedPattern(username []string, timestamp []int, website []string) []
 		}
 	}
 
-	// make 3 sequences
-	//var threeSeqs = make(map[string]int)
-
-	for username, visits := range usernameToVisits {
-		log.Println("chimken", username, visits)
+	var highestCount = 0
+	var mostVisitedPattenSlice = make([]string, 3)
+	for _, websites := range usernameToVisits {
+		for i := 0; i < len(websites); i++ {
+			for j := i + 1; j < len(websites); j++ {
+				if websites[j] == websites[i] {
+					continue
+				}
+				for k := j + 1; k < len(websites); k++ {
+					if websites[k] == websites[j] {
+						continue
+					}
+					var seq = fmt.Sprintf("%s,%s,%s",websites[i],websites[j],websites[k])
+					var count, _ = threeSeqs[seq]
+					var newCount = count + 1
+					threeSeqs[seq] = newCount
+					if newCount > highestCount {
+						highestCount = newCount
+						mostVisitedPattenSlice = []string{websites[i], websites[j], websites[k]}
+					}
+				}
+			}
+		}
 	}
 
-
-	return []string{}
+	return mostVisitedPattenSlice
 }
 
 func main() {
