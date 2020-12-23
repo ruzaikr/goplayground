0bcda38a29a2d7408395b4c477e94e1781ac8819
DESKTOP-MJJG10R\ruzai <ruzaik.rafeek@gmail.com>
2020-12-23 21:48:06 +0800

    Subtree of Another Tree - Approach #1

--- main.go
+++ main.go
@@ -1,93 +1,44 @@
-// This example demonstrates a priority queue built using the heap interface.
 package main
 
-import (
-	"container/heap"
-	"sort"
-)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
 
-// An Item is something we manage in a priority queue.
-type Item struct {
-	priority int // The priority of the item in the queue.
-	// The index is needed by update and is maintained by the heap.Interface methods.
-	index int // The index of the item in the heap.
-}
-
-// A PriorityQueue implements heap.Interface and holds Items.
-type PriorityQueue []*Item
-
-func (pq PriorityQueue) Len() int { return len(pq) }
-
-func (pq PriorityQueue) Less(i, j int) bool {
-	// We want Pop to give us the lowest, not highest, priority so we use lesser than here.
-	return pq[i].priority < pq[j].priority
-}
-
-func (pq PriorityQueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
-}
-
-func (pq *PriorityQueue) Push(x interface{}) {
-	n := len(*pq)
-	item := x.(*Item)
-	item.index = n
-	*pq = append(*pq, item)
-}
-
-func (pq *PriorityQueue) Pop() interface{} {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	old[n-1] = nil  // avoid memory leak
-	item.index = -1 // for safety
-	*pq = old[0 : n-1]
-	return item
-}
-
-func minMeetingRooms(intervals [][]int) int {
-	if len(intervals) < 1 {
-		return 0
-	} else if len(intervals) == 1 {
-		return 1
+func preOrderTraversal(a *TreeNode, seq string) string {
+	if a == nil {
+		return seq
 	}
 
-	sort.Slice(intervals, func(i, j int) bool {
-		if intervals[i][0] < intervals[j][0] {
-			return true
-		} else if intervals[i][0] == intervals[j][0] {
-			return intervals[i][1] < intervals[j][1]
-		}
-		return false
-	})
+	seq = fmt.Sprintf("%s,-%d", seq, a.Val)
 
-	var pq = make(PriorityQueue, 1)
-	pq[0] = &Item{
-		priority: intervals[0][1],
-		index:    0,
+	if a.Left != nil {
+		seq = preOrderTraversal(a.Left, seq)
+	}else {
+		seq = fmt.Sprintf("%s,%s", seq, "lnull")
 	}
 
-	heap.Init(&pq)
-
-	for i := 1; i < len(intervals); i++ {
-		if intervals[i][0] >= pq[0].priority {
-			heap.Pop(&pq)
-		}
-		var item = &Item{
-			priority: intervals[i][1],
-			index:    0,
-		}
-		heap.Push(&pq, item) // why not `pq.Push(item)`?
+	if a.Right != nil {
+		seq = preOrderTraversal(a.Right, seq)
+	}else {
+		seq = fmt.Sprintf("%s,%s", seq, "rnull")
 	}
 
-	return len(pq)
+	return seq
 }
 
-// This example creates a PriorityQueue with some items, adds and manipulates an item,
-// and then removes the items in priority order.
+func isSubtree(s *TreeNode, t *TreeNode) bool {
+	var seqS = preOrderTraversal(s, "")
+	fmt.Println("seqS:",seqS)
+	var seqT = preOrderTraversal(t, "")
+	fmt.Println("seqT:",seqT)
+	return strings.Contains(seqS, seqT)
+}
+
 func main() {
 
 }
-
-//
