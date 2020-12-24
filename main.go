30b4bef2986f6df394ac6a251b2da975e84fcd84
DESKTOP-MJJG10R\ruzai <ruzaik.rafeek@gmail.com>
2020-12-24 18:46:33 +0800

    working solution

--- main.go
+++ main.go
@@ -8,19 +8,19 @@ import (
 // =================
 //
 
-func helper(grid [][]int, r int, c int, oldColor int, newColor int, visited map[string]bool) [][]int {
+func helper(grid [][]int, r int, c int, oldColor int, visited map[string]bool, border [][]int) ([][]int, [][]int) {
 	var key = fmt.Sprintf("%d,%d", r, c)
 	if _, exists := visited[key]; exists {
-		return grid
+		return grid, border
 	}
 	visited[key] = true // mark as visited
 
 	if r == -1 || r == len(grid) || c == -1 || c == len(grid[r]) { // check if r,c is out of bounds
-		return grid
+		return grid, border
 	}
 
 	if grid[r][c] != oldColor {
-		return grid
+		return grid, border
 	}
 
 	var leftc = c - 1
@@ -33,22 +33,33 @@ func helper(grid [][]int, r int, c int, oldColor int, newColor int, visited map[
 		grid[r][rightc] != oldColor ||
 		grid[upr][c] != oldColor ||
 		grid[downr][c] != oldColor {
-		grid[r][c] = newColor
+		border = append(border, []int{r, c})
 	}
 
-	helper(grid, r, leftc, oldColor, newColor, visited)
-	helper(grid, r, rightc, oldColor, newColor, visited)
-	helper(grid, upr, c, oldColor, newColor, visited)
-	helper(grid, downr, c, oldColor, newColor, visited)
+	grid, border = helper(grid, r, leftc, oldColor, visited, border)
+	grid, border = helper(grid, r, rightc, oldColor, visited, border)
+	grid, border = helper(grid, upr, c, oldColor, visited, border)
+	grid, border = helper(grid, downr, c, oldColor, visited, border)
 
-	return grid
+	return grid, border
 }
 
 func colorBorder(grid [][]int, r0 int, c0 int, color int) [][]int {
 
 	// @todo: case where r0,c0 does not have any adjacent squares with the same color
 
-	return helper(grid, r0, c0, grid[r0][c0], color, make(map[string]bool))
+	if len(grid) < 1 || len(grid[0]) < 1 {
+		return grid
+	}
+
+	var border = make([][]int, 0, len(grid)*len(grid[0]))
+	grid, border = helper(grid, r0, c0, grid[r0][c0], make(map[string]bool), border)
+
+	for i := 0; i < len(border); i++ {
+		grid[border[i][0]][border[i][1]] = color
+	}
+
+	return grid
 }
 
 func main() {
