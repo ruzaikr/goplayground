bcfaa85570c2d3d6df4babbd39fc99c5e10f5782
ruzaik.rafeek <ruzaik.rafeek@lalamove.com>
2020-12-28 19:28:25 +0800

    First solution

--- main.go
+++ main.go
@@ -2,63 +2,56 @@ package main
 
 import (
 	"fmt"
-	"sync"
 )
 
-type HitCounter struct {
-	mu   sync.Mutex
-	hits map[int]int
-}
-
-/** Initialize your data structure here. */
-func Constructor() HitCounter {
-	return HitCounter{
-		hits: make(map[int]int),
+func getMin(a int, b int) int {
+	if a < b {
+		return a
 	}
+	return b
 }
 
-/** Record a hit.
-  @param timestamp - The current timestamp (in seconds granularity). */
-func (this *HitCounter) Hit(timestamp int) {
-	this.mu.Lock()
-	this.hits[timestamp] = this.hits[timestamp] + 1
-	this.mu.Unlock()
-}
-
-/** Return the number of hits in the past 5 minutes.
-  @param timestamp - The current timestamp (in seconds granularity). */
-func (this *HitCounter) GetHits(timestamp int) int {
-	this.mu.Lock()
-	defer this.mu.Unlock()
-	var start = timestamp - 299
-	if start < 0 {
-		start = 1
+func isSquare(i int, j int, sideLen int, matrix [][]byte) bool {
+	for k := i; k <= sideLen; k++ {
+		for l := j; l <= sideLen; l++ {
+			fmt.Println(matrix[k][l])
+			if matrix[k][l] != 49 {
+				return false
+			}
+		}
 	}
-	var noOfHits int
-	for i := start; i <= timestamp; i++ {
-		noOfHits = noOfHits + this.hits[i]
-	}
-	return noOfHits
+	return true
 }
 
-/**
- * Your HitCounter object will be instantiated and called as such:
- * obj := Constructor();
- * obj.Hit(timestamp);
- * param_2 := obj.GetHits(timestamp);
- */
+func maximalSquare(matrix [][]byte) int {
+	if len(matrix) < 1 || len(matrix[0]) < 1{
+		return 0
+	}
+
+	var lenY = len(matrix)
+	var lenX = len(matrix[0])
+
+	var maxSquareSide = getMin(lenX, lenY)
+
+	fmt.Println("maxSquareSide = ", maxSquareSide)
+
+	outermost:
+	for maxSquareSide > 0 {
+		for y := 0; y < (lenY - maxSquareSide + 1); y++ {
+			for x := 0; x < (lenX - maxSquareSide + 1); x++ {
+				if isSquare(y, x, maxSquareSide, matrix) {
+					fmt.Println("y = ", y, " x = ", x)
+					break outermost
+				}
+			}
+		}
+		maxSquareSide = maxSquareSide - 1
+	}
+
+	return maxSquareSide*maxSquareSide
+}
+
 
 func main() {
-	var myHitCounter = Constructor()
-	myHitCounter.Hit(1)
-	myHitCounter.Hit(2)
-	myHitCounter.Hit(3)
-	fmt.Println(myHitCounter.GetHits(4))
-
-	for i := 0; i < 999; i++ {
-		go myHitCounter.Hit(300)
-	}
-
-	fmt.Println(myHitCounter.GetHits(300))
-	fmt.Println(myHitCounter.GetHits(301))
+	fmt.Println("Hello World")
 }
