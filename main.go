e4cfcd3092aa83861c8f1b9b7778f3f38daf5236
Ruzaik Rafeek <ruzaik.rafeek@lalamove.com>
2021-01-10 12:23:18 +0800

    initial commit

--- main.go
+++ main.go
@@ -1,79 +1,151 @@
+// To execute Go code, please declare a func main() in a package "main"
+
 package main
 
 import (
 	"fmt"
-	"log"
+	"strings"
 )
 
-func helper(r int, c int, matrix [][]int, visited1s map[string]bool, sizeOfRiver int) (map[string]bool, int) {
-	log.Println("helper r,c-> ", r, c)
+func main() {
+	var inputArr = []string{"P1", "D1", "P2", "D2"}  // true
+	var inputArr2 = []string{"P1", "P2", "D1", "D2"} // true
+	var inputArr3 = []string{"P2", "P1", "D1", "D2"} // true
+	var inputArr4 = []string{"P1", "D1", "P2"}       // false
+	var inputArr5 = []string{"D1", "P1"}             // false
+	var inputArr6 = []string{"P1", "D1", "P1", "D2"} // false
+	var inputArr7 = []string{"P1", "D1", "P1", "D1"} // false
 
-	if r < 0 || c < 0 || r >= len(matrix) || c >= len(matrix[0]){
-		return visited1s, sizeOfRiver
-	}
-
-	var val = matrix[r][c]
-
-	if val == 0 || visited1s[fmt.Sprintf("%d,%d",r,c)]{
-		return visited1s, sizeOfRiver
-	}
-
-	sizeOfRiver++ // val must be 1 if not 0
-	visited1s[fmt.Sprintf("%d,%d",r,c)] = true
-
-	var upr = r + 1
-	var downr = r - 1
-	var rightc = c + 1
-	var leftc = c - 1
-
-	visited1s, sizeOfRiver = helper(upr, c, matrix, visited1s, sizeOfRiver)
-	visited1s, sizeOfRiver = helper(downr, c, matrix, visited1s, sizeOfRiver)
-	visited1s, sizeOfRiver = helper(r, rightc, matrix, visited1s, sizeOfRiver)
-	visited1s, sizeOfRiver = helper(r, leftc, matrix, visited1s, sizeOfRiver)
-
-	return visited1s, sizeOfRiver
-
+	fmt.Println(determineScheduleIsValid(inputArr))
+	fmt.Println(determineScheduleIsValid(inputArr2))
+	fmt.Println(determineScheduleIsValid(inputArr3))
+	fmt.Println(determineScheduleIsValid(inputArr4))
+	fmt.Println(determineScheduleIsValid(inputArr5))
+	fmt.Println(determineScheduleIsValid(inputArr6))
+	fmt.Println(determineScheduleIsValid(inputArr7))
 }
 
-func RiverSizes(matrix [][]int) []int {
-	var noRows = len(matrix)
-	if noRows < 1 {
-		return []int{}
-	}
-	var noCols = len(matrix[0])
+/*
 
-	var visited1s = make(map[string]bool)
-	var riverSizes = make([]int, 0)
+A valid delivery is if the dropoff is after the pickup
 
-	for r := 0; r < noRows; r++ {
-		for c := 0; c < noCols; c++ {
-			log.Println("iteration-------------")
 
-			if matrix[r][c] == 0 || visited1s[fmt.Sprintf("%d,%d",r,c)] {
-				continue
+P1, D1, P2, D2 => valid
+P1, P2, D1, D2 => valid
+P2, P1, D1, D2 => valid
+
+P1, D1, P1, D1 => valid
+
+
+P1, D1, P2 => invalid
+D1, P1 => invalid
+P1, D1, P1, D2 => invalid
+
+
+
+
+Your goal is to determine if a schedule is valid.
+
+input parameter : ["P1", "D1...
+
+
+*/
+
+func determineScheduleIsValid(inputArr []string) bool {
+	var mapPickupsToDeliveries = make(map[string]bool)
+	for i := 0; i < len(inputArr); i++ {
+		if strings.HasPrefix(inputArr[i], "P") {
+			var indexOfPickup = inputArr[i][1:]
+			if mapPickupsToDeliveries[indexOfPickup] {
+				return false
 			}
-
-			// current element is a 1 that has not been processed
-			var sizeOfRiver int
-			visited1s, sizeOfRiver = helper(r, c, matrix, visited1s, sizeOfRiver)
-
-			riverSizes = append(riverSizes, sizeOfRiver)
-
+			mapPickupsToDeliveries[indexOfPickup] = false
+		}
+		if strings.HasPrefix(inputArr[i], "D") {
+			var indexOfDelivery = inputArr[i][1:]
+			var val, exists = mapPickupsToDeliveries[indexOfDelivery]
+			if exists && val == false {
+				delete(mapPickupsToDeliveries, indexOfDelivery)
+			} else {
+				return false
+			}
 		}
 	}
-	return riverSizes
+
+	return len(mapPickupsToDeliveries) == 0
 }
 
-func main() {
-	log.Println("hi")
+// O(n)
+// O(n)
 
-	var matrix = [][]int{
-		{1, 0, 0, 1, 0},
-		{1, 0, 1, 0, 0},
-		{0, 0, 1, 0, 1},
-		{1, 0, 1, 0, 1},
-		{1, 0, 1, 1, 0},
-	}
+// func determineScheduleIsValid() bool {
+//   for i := 0; i < len(inputArr); i++ {
+//     if strings.HasPrefix(inputArr[i], "P") {
+//       var pickupIsValid bool
+//       var indexOfPickup = inputArr[i][1:]
+//       for j := i + 1; j < len(inputArr); j++ {
+//         if strings.HasPrefix(inputArr[j], "D") {
+//           var indexOfDelivery = inputArr[j][1:]
+//           if indexOfPickup == indexOfDelivery {
+//             pickupIsValid = true
+//           }
+//         }
+//       }
+//       if !pickupIsValid {
+//         return false
+//       }
+//     }
+//   }
+//   return true
+// }
 
-	log.Println(RiverSizes(matrix))
+// invalid case [P1, D2, P2, D1]  invalid
+// hash table { 1: false,
+//
+// invalid case [P1, D1, P2, D3]  invalid
+// hash table { 1: true, 2: false
+//
+
+// time complexity: [P1, P2... P8]
+// O(n^2)
+
+/*
+
+Given n, generate all valid unique schedules
+
+if n = 1
+  P1, D1
+
+if n = 2
+  P1, D1, P2, D2
+  P1, P2, D1, D2
+  P1, P2, D2, D1
+  P2, P1, D1, D2
+  P2, P1, D2, D1
+
+[P/D 1-n/2 ], [ ], [ ], [ ]
+n/2
+
+----------
+
+
+n = 2
+choices = [P1, P2]
+
+    choices= [P2, D1]  (P1)
+
+         choices [D1, D2]  (P1, P2)
+
+
+            choices [D2]   (P1, P2, D1)
+
+                 choices [] (P1, P2, D1, D2)
+
+
+
+*/
+
+func generateAllValidSchedules(n int) {
+	var result = make([]string, 0)
+
 }
