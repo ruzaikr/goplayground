4aaa062e28f55c745b8e650171b864c172f27e70
DESKTOP-MJJG10R\ruzai <ruzaik.rafeek@gmail.com>
2021-01-11 18:25:58 +0800

    Working solution

--- main.go
+++ main.go
@@ -1,151 +1,99 @@
-// To execute Go code, please declare a func main() in a package "main"
-
 package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func main() {
-	var inputArr = []string{"P1", "D1", "P2", "D2"}  // true
-	var inputArr2 = []string{"P1", "P2", "D1", "D2"} // true
-	var inputArr3 = []string{"P2", "P1", "D1", "D2"} // true
-	var inputArr4 = []string{"P1", "D1", "P2"}       // false
-	var inputArr5 = []string{"D1", "P1"}             // false
-	var inputArr6 = []string{"P1", "D1", "P1", "D2"} // false
-	var inputArr7 = []string{"P1", "D1", "P1", "D1"} // false
+type Coords struct {
+	Row      int
+	Col      int
+	Distance int
+}
 
-	fmt.Println(determineScheduleIsValid(inputArr))
-	fmt.Println(determineScheduleIsValid(inputArr2))
-	fmt.Println(determineScheduleIsValid(inputArr3))
-	fmt.Println(determineScheduleIsValid(inputArr4))
-	fmt.Println(determineScheduleIsValid(inputArr5))
-	fmt.Println(determineScheduleIsValid(inputArr6))
-	fmt.Println(determineScheduleIsValid(inputArr7))
+func areCoordsValid(coords Coords, maxRow int, maxCol int) bool {
+	if coords.Row < 0 || coords.Col < 0 {
+		return false
+	}
+	if coords.Row > maxRow || coords.Col > maxCol {
+		return false
+	}
+	return true
 }
 
 /*
-
-A valid delivery is if the dropoff is after the pickup
-
-
-P1, D1, P2, D2 => valid
-P1, P2, D1, D2 => valid
-P2, P1, D1, D2 => valid
-
-P1, D1, P1, D1 => valid
-
-
-P1, D1, P2 => invalid
-D1, P1 => invalid
-P1, D1, P1, D2 => invalid
-
-
-
-
-Your goal is to determine if a schedule is valid.
-
-input parameter : ["P1", "D1...
-
-
+Question:
+Find the nearest hotels within a range [min, max] in a 2D  grid, where the cells with the value 1 represent part of a
+road, a value bigger than 1 is the price of a room per night and an empty cell no road or hotel.
 */
+func getNearestHotelsInRange(grid [][]int, startingCoords Coords, minPrice int, maxPrice int) []int {
+	if len(grid) < 1 || len(grid[0]) < 1 {
+		return []int{}
+	}
 
-func determineScheduleIsValid(inputArr []string) bool {
-	var mapPickupsToDeliveries = make(map[string]bool)
-	for i := 0; i < len(inputArr); i++ {
-		if strings.HasPrefix(inputArr[i], "P") {
-			var indexOfPickup = inputArr[i][1:]
-			if mapPickupsToDeliveries[indexOfPickup] {
-				return false
+	var nearestHotelsInRange = make([]int, 0)
+	var enqueued = make(map[string]bool)
+	var bfsQueue = []Coords{startingCoords}
+
+	var currDistance = 0
+	for len(bfsQueue) > 0 {
+
+		// dequeue
+		var currCoords = bfsQueue[0]
+		bfsQueue = bfsQueue[1:]
+
+		if currDistance < currCoords.Distance { // we have finished exploring the next current distance level
+			currDistance = currCoords.Distance
+			if len(nearestHotelsInRange) > 0 {
+				return nearestHotelsInRange
 			}
-			mapPickupsToDeliveries[indexOfPickup] = false
 		}
-		if strings.HasPrefix(inputArr[i], "D") {
-			var indexOfDelivery = inputArr[i][1:]
-			var val, exists = mapPickupsToDeliveries[indexOfDelivery]
-			if exists && val == false {
-				delete(mapPickupsToDeliveries, indexOfDelivery)
-			} else {
-				return false
+
+		var cellValue = grid[currCoords.Row][currCoords.Col]
+
+		if cellValue > 1 && cellValue >= minPrice && cellValue <= maxPrice {
+			nearestHotelsInRange = append(nearestHotelsInRange, grid[currCoords.Row][currCoords.Col])
+		}
+
+		if cellValue == 0 {
+			continue
+		}
+
+		var newDistance = currCoords.Distance + 1
+		var northCoords = Coords{Row: currCoords.Row - 1, Col: currCoords.Col, Distance: newDistance}
+		var eastCoords = Coords{Row: currCoords.Row, Col: currCoords.Col + 1, Distance: newDistance}
+		var southCoords = Coords{Row: currCoords.Row + 1, Col: currCoords.Col, Distance: newDistance}
+		var westCoords = Coords{Row: currCoords.Row, Col: currCoords.Col - 1, Distance: newDistance}
+		var neighbors = []Coords{northCoords, eastCoords, southCoords, westCoords}
+
+		for i := 0; i < len(neighbors); i++ {
+			var key = fmt.Sprintf("%d,%d", neighbors[i].Col, neighbors[i].Row)
+			if areCoordsValid(neighbors[i], len(grid)-1, len(grid[0])-1) && !enqueued[key] {
+				bfsQueue = append(bfsQueue, neighbors[i]) // enqueue
+				enqueued[key] = true
 			}
 		}
 	}
 
-	return len(mapPickupsToDeliveries) == 0
+	return nearestHotelsInRange
 }
 
-// O(n)
-// O(n)
+func main() {
+	var startingCol = 3
+	var startingRow = 2
 
-// func determineScheduleIsValid() bool {
-//   for i := 0; i < len(inputArr); i++ {
-//     if strings.HasPrefix(inputArr[i], "P") {
-//       var pickupIsValid bool
-//       var indexOfPickup = inputArr[i][1:]
-//       for j := i + 1; j < len(inputArr); j++ {
-//         if strings.HasPrefix(inputArr[j], "D") {
-//           var indexOfDelivery = inputArr[j][1:]
-//           if indexOfPickup == indexOfDelivery {
-//             pickupIsValid = true
-//           }
-//         }
-//       }
-//       if !pickupIsValid {
-//         return false
-//       }
-//     }
-//   }
-//   return true
-// }
+	var grid = [][]int{
+		{6, 4, 0, 15, 7},
+		{2, 4, 1, 0, 1},
+		{6, 1, 1, 1, 1},
+		{9, 1, 1, 1, 0},
+		{0, 1, 0, 1, 0},
+	}
 
-// invalid case [P1, D2, P2, D1]  invalid
-// hash table { 1: false,
-//
-// invalid case [P1, D1, P2, D3]  invalid
-// hash table { 1: true, 2: false
-//
+	//  var grid = [][]int{
+	//     {2, 8, 0},
+	//     {9, 1, 1},
+	//     {3, 1, 1},
+	// }
 
-// time complexity: [P1, P2... P8]
-// O(n^2)
-
-/*
-
-Given n, generate all valid unique schedules
-
-if n = 1
-  P1, D1
-
-if n = 2
-  P1, D1, P2, D2
-  P1, P2, D1, D2
-  P1, P2, D2, D1
-  P2, P1, D1, D2
-  P2, P1, D2, D1
-
-[P/D 1-n/2 ], [ ], [ ], [ ]
-n/2
-
-----------
-
-
-n = 2
-choices = [P1, P2]
-
-    choices= [P2, D1]  (P1)
-
-         choices [D1, D2]  (P1, P2)
-
-
-            choices [D2]   (P1, P2, D1)
-
-                 choices [] (P1, P2, D1, D2)
-
-
-
-*/
-
-func generateAllValidSchedules(n int) {
-	var result = make([]string, 0)
-
+	fmt.Println(getNearestHotelsInRange(grid, Coords{Row: startingRow, Col: startingCol, Distance: 0}, 12, 15))
 }
